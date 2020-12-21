d79a6526e27e248989fe64ead747425336201ed8
fmanen <fe.manen@gmail.com>
2020-12-21 17:17:58 -0300

    Some changes on templates

--- OpenCSMap/settings.py
+++ OpenCSMap/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '$y)su&jkz$@_$=6k#d-%haqu6tz#6+-%&qf&9%@ybgvz7q#=i@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['10.0.106.3', 'opencsmap.dcc.uchile.cl', '*']
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
 ]
 
 MIDDLEWARE = [
